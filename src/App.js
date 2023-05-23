3cf887d8a21538a7dc48e73f98187ff95adfab30
Monica Gerard <mlgvla@gmail.com>
2023-05-23 18:16:31 -0400

    starts here

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./index.css"
-import { characters } from "./db"
-import CharacterList from "./components/CharacterList"
-import Header from "./components/Header"
+
 
 const App = () => {
    return (
       <div>
-         <Header />
-         <CharacterList characters={characters} />
+        
       </div>
    )
 }
