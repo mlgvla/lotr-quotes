c6e296d376e3309792ef5701da8aa4f61f51e379
Monica Gerard <mlgvla@gmail.com>
2023-05-23 16:29:05 -0400

    start here

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import CharacterList from "./components/CharacterList.js";
-import { characters } from "./db.js";
-import Header from "./components/Header.js";
 import "./index.css"
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <CharacterList characters={characters} />
+     
     </div>
   );
 };
--- src/components/Quote.js
+++
@@ -1,5 +0,0 @@
-function Quote({ quote }) {
-   return <li className="quote">{quote}</li>
-}
-
-export default Quote
